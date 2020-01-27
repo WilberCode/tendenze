f99a4dba2dd94ed7ee841448918bdcfad285ef7e
Wilber <parionahuaraca@gmail.com>
2020-01-27 13:15:16 -0500

    se agrego pwa

--- js/scripts.js
+++ js/scripts.js
@@ -264,3 +264,10 @@ window.onload = () => {
     _parallaxCategorias();
   }
 }
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+} 
